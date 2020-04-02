f1f30e857c905eef9a05fdbcbece76406a4b5d5c
pm0824 <maneprachi824@gmail.com>
2020-04-02 23:09:11 +0530

    JS quiz

---
+++ Readme.md
@@ -0,0 +1,13 @@
+1. What is JavaScript?
+JavaScript was initially created to “make web pages alive”.
+
+Scripts are provided and executed as plain text. They don’t need special preparation or compilation to run.
+
+2. Why is it called JavaScript?
+When JavaScript was created, it initially had another name: “LiveScript”. But Java was very popular at that time, so it was decided that positioning a new language as a “younger brother” of Java would help.
+
+But as it evolved, JavaScript became a fully independent language with its own specification called ECMAScript, and now it has no relation to Java at all.
+
+Today, JavaScript can execute not only in the browser, but also on the server, or actually on any device that has a special program called the JavaScript engine.
+
+Here I have developed the basic quiz system using Javascript.
---
+++ js/script.js
@@ -0,0 +1,36 @@
+function submitAns(){
+    var total=5;
+    var score=0;
+
+    //get user input
+    var q1=document.forms['quizForm']['q1'].value;
+    var q2=document.forms['quizForm']['q2'].value;
+    var q3=document.forms['quizForm']['q3'].value;
+    var q4=document.forms['quizForm']['q4'].value;
+    var q5=document.forms['quizForm']['q5'].value;
+    
+    //validation
+    
+    for(var i=1;i<=total;i++){
+        
+        if(eval('q'+i)==null || eval('q'+i)==''){
+            alert('missed que '+i);
+            return false;
+        }
+    }
+    //set correct answers
+    var ans=["d","c","a","b","d"];
+
+    //check answers
+    for(var i=0;i<total;i++){
+        
+        if(eval('q'+(i+1))==ans[i]){
+            score++;
+        }
+    }
+    //Display Results
+    var result=document.getElementById('results');
+    result.innerHTML="<h3>You scored <span>"+score+"</span></h3>"
+    alert("Your Score is "+score);
+    return false;
+}
